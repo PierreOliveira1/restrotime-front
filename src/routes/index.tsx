8937e9c928d90276a25dfe374c7f4f5e6c25cfaf
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-30 23:53:26 -0300

    feat: configured routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Header } from '@/components/common/Header';
 import { Restaurants } from './Restaurants';
+import { Restaurant } from './Restaurant';
+import { SearchRestaurants } from './SearchRestaurants';
 
 const routes = createBrowserRouter([
 	{
@@ -10,6 +12,14 @@ const routes = createBrowserRouter([
 			{
 				path: '/',
 				element: <Restaurants />,
+			},
+			{
+				path: '/restaurante/novo',
+				element: <Restaurant />,
+			},
+			{
+				path: '/restaurants/search',
+				element: <SearchRestaurants />,
 			},
 		],
 	},
